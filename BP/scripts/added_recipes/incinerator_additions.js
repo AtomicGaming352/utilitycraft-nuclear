dc961d0d15625e901e6cc6f4cae7153e29ca0b60
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-03-13 18:00:22 +0800

    Update incinerator_additions.js

--- BP/scripts/added_recipes/incinerator_additions.js
+++ BP/scripts/added_recipes/incinerator_additions.js
@@ -2,7 +2,11 @@ import { system, world } from "@minecraft/server";
 
 world.afterEvents.worldLoad.subscribe(() => {
     const newRecipes = {
-        "utilitycraft:ryno_raw_lead": { output: "utilitycraft:ryno_lead_ingot" }
+        "utilitycraft:ryno_raw_lead": { output: "utilitycraft:ryno_lead_ingot" },
+        "utilitycraft:ryno_lead_dust": { output: "utilitycraft:ryno_lead_ingot" },
+        "utilitycraft:ryno_raw_uranium": { output: "utilitycraft:ryno_uranium_ingot" },
+        "utilitycraft:ryno_uranium_dust": { output: "utilitycraft:ryno_uranium_ingot" },
+        "utilitycraft:ryno_sulfuric_saltpeter_canister": { output: "utilitycraft:ryno_sulfuric_acid_canister" }
     };
 
     system.sendScriptEvent("utilitycraft:register_furnace_recipe", JSON.stringify(newRecipes));
