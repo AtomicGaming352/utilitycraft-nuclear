337584ad0d733ab37589060192ea257083e3523a
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-03-14 01:53:43 +0800

    Update crusher_additions.js

--- BP/scripts/added_recipes/crusher_additions.js
+++ BP/scripts/added_recipes/crusher_additions.js
@@ -2,15 +2,15 @@ import { system, world } from "@minecraft/server";
 
 world.afterEvents.worldLoad.subscribe(() => {
     const newRecipes = {
-      "utilitycraft:ryno_deepslate_lead_chunk": { output: "utlitycraft:ryno_raw_lead", required: 1 },
-      "utilitycraft:ryno_deepslate_uranium_chunk": { output: "utlitycraft:ryno_raw_uranium", required: 1 },
-      "utilitycraft:ryno_deepslate_vanadium_chunk": { output: "utlitycraft:ryno_raw_vanadium", required: 1 },
-      "utilitycraft:ryno_vanadium_chunk": { output: "utlitycraft:ryno_raw_vanadium", required: 1 },
-      "utlitycraft:ryno_raw_lead": { output: "utlitycraft:ryno_lead_dust", amount: 2, required: 1 },
-      "utilitycraft:ryno_raw_uranium": { output: "utlitycraft:ryno_uranium_dust", amount: 2, required: 1 },
-      "utilitycraft:ryno_raw_vanadium": { output: "utlitycraft:ryno_vanadium_dust", amount: 2, required: 1 },
-      "utilitycraft:ryno_sulfur_chunk": { output: "utlitycraft:ryno_sulfur_dust", amount: 4, required: 1 },
-      "utilitycraft:ryno_deepslate_sulfur_chunk": { output: "utlitycraft:ryno_sulfur_dust", amount: 4, required: 1 },
+      "utilitycraft:ryno_deepslate_lead_chunk": { output: "utilitycraft:ryno_raw_lead", required: 1 },
+      "utilitycraft:ryno_deepslate_uranium_chunk": { output: "utilitycraft:ryno_raw_uranium", required: 1 },
+      "utilitycraft:ryno_deepslate_vanadium_chunk": { output: "utilitycraft:ryno_raw_vanadium", required: 1 },
+      "utilitycraft:ryno_vanadium_chunk": { output: "utilitycraft:ryno_raw_vanadium", required: 1 },
+      "utlitycraft:ryno_raw_lead": { output: "utilitycraft:ryno_lead_dust", amount: 2, required: 1 },
+      "utilitycraft:ryno_raw_uranium": { output: "utilitycraft:ryno_uranium_dust", amount: 2, required: 1 },
+      "utilitycraft:ryno_raw_vanadium": { output: "utilitycraft:ryno_vanadium_dust", amount: 2, required: 1 },
+      "utilitycraft:ryno_sulfur_chunk": { output: "utilitycraft:ryno_sulfur_dust", amount: 4, required: 1 },
+      "utilitycraft:ryno_deepslate_sulfur_chunk": { output: "utilitycraft:ryno_sulfur_dust", amount: 4, required: 1 },
     };
 
     system.sendScriptEvent("utilitycraft:register_crusher_recipe", JSON.stringify(newRecipes));
