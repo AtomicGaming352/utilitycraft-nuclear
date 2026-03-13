c54b58d69e6c51da1766f6439f81ae511ffd3324
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-03-13 18:01:13 +0800

    Update press_additions.js

--- BP/scripts/added_recipes/press_additions.js
+++ BP/scripts/added_recipes/press_additions.js
@@ -3,6 +3,7 @@ import { system, world } from "@minecraft/server";
 world.afterEvents.worldLoad.subscribe(() => {
     const newRecipes = {
         "utilitycraft:ryno_lead_ingot": { output: "utlitycraft:ryno_lead_plate", required: 1 },
+        "utilitycraft:ryno_lead_plate": { output: "utlitycraft:ryno_lead_plating", required: 3 },
     };
 
     system.sendScriptEvent("utilitycraft:register_press_recipe", JSON.stringify(newRecipes));
