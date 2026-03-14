57fd5804bb78832a5a2046b3ce57ee493f5a827e
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-03-14 18:24:01 +0800

    Update reactor.js

--- BP/scripts/config/recipes/reactor.js
+++ BP/scripts/config/recipes/reactor.js
@@ -1,10 +1,10 @@
 export const basicnuclearFuels = [
-    { id: "utilitycraft:ryno_refined_uranium", de: 200000 },
+    { id: "utilitycraft:ryno_refined_uranium", de: 20000 },
     { id: "utilitycraft:ryno_nuclear_fuel", de: 500000 },
 ];
 
 export const advancednuclearFuels = [
-    { id: "utilitycraft:ryno_refined_uranium", de: 200000 },
+    { id: "utilitycraft:ryno_refined_uranium", de: 20000 },
     { id: "utilitycraft:ryno_nuclear_fuel", de: 500000 },
     { id: "utilitycraft:ryno_coolant_nuclear_fuel", de: 2000000 },
 ];
