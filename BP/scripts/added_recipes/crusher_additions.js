a7f7a4faa47bd41c9932cba29c123a47b7fc1cbc
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-03-20 13:36:39 +0800

    Update crusher_additions.js

--- BP/scripts/added_recipes/crusher_additions.js
+++ BP/scripts/added_recipes/crusher_additions.js
@@ -9,6 +9,10 @@ world.afterEvents.worldLoad.subscribe(() => {
       "utilitycraft:ryno_raw_lead": { output: "utilitycraft:ryno_lead_dust", amount: 2, required: 1 },
       "utilitycraft:ryno_raw_uranium": { output: "utilitycraft:ryno_uranium_dust", amount: 2, required: 1 },
       "utilitycraft:ryno_raw_vanadium": { output: "utilitycraft:ryno_vanadium_dust", amount: 2, required: 1 },
+      "utilitycraft:ryno_lead_plate": { output: "utilitycraft:ryno_lead_dust", amount: 1, required: 1 },
+      "utilitycraft:ryno_lead_ingot": { output: "utilitycraft:ryno_lead_dust", amount: 1, required: 1 },
+      "utilitycraft:ryno_uranium_ingot": { output: "utilitycraft:ryno_uranium_dust", amount: 1, required: 1 },
+      "utilitycraft:ryno_vanadium_ingot": { output: "utilitycraft:ryno_vanadium_dust", amount: 1, required: 1 },
       "utilitycraft:ryno_sulfur_chunk": { output: "utilitycraft:ryno_sulfur_dust", amount: 4, required: 1 },
       "utilitycraft:ryno_deepslate_sulfur_chunk": { output: "utilitycraft:ryno_sulfur_dust", amount: 4, required: 1 },
     };
