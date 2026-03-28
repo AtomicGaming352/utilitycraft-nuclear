0780254315dd5cf1c766f8bb138439d6bd93e115
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-03-28 15:22:15 +0800

    Adjust radiation damage thresholds and intervals

--- BP/scripts/machinery/radiation.js
+++ BP/scripts/machinery/radiation.js
@@ -20,8 +20,73 @@ system.runInterval(() => {
     let protection = radProtection.getScore(player) ?? 0;
 
     // Condition
-    if (radiation >= 40 && protection < 12) {
+    if (radiation >= 32 && protection < 12) {
       player.applyDamage(1);
     }
   }
-}, 60);
+}, 400);
+system.runInterval(() => {
+  for (const player of world.getPlayers()) {
+
+    // Get scores safely
+    let radiation = rad.getScore(player) ?? 0;
+    let protection = radProtection.getScore(player) ?? 0;
+
+    // Condition
+    if (radiation >= 48 && protection < 12) {
+      player.applyDamage(1);
+    }
+  }
+}, 320);
+system.runInterval(() => {
+  for (const player of world.getPlayers()) {
+
+    // Get scores safely
+    let radiation = rad.getScore(player) ?? 0;
+    let protection = radProtection.getScore(player) ?? 0;
+
+    // Condition
+    if (radiation >= 64 && protection < 12) {
+      player.applyDamage(1);
+    }
+  }
+}, 240);
+system.runInterval(() => {
+  for (const player of world.getPlayers()) {
+
+    // Get scores safely
+    let radiation = rad.getScore(player) ?? 0;
+    let protection = radProtection.getScore(player) ?? 0;
+
+    // Condition
+    if (radiation >= 80 && protection < 12) {
+      player.applyDamage(2);
+    }
+  }
+}, 240);
+system.runInterval(() => {
+  for (const player of world.getPlayers()) {
+
+    // Get scores safely
+    let radiation = rad.getScore(player) ?? 0;
+    let protection = radProtection.getScore(player) ?? 0;
+
+    // Condition
+    if (radiation >= 96 && protection < 12) {
+      player.applyDamage(2);
+    }
+  }
+}, 160);
+system.runInterval(() => {
+  for (const player of world.getPlayers()) {
+
+    // Get scores safely
+    let radiation = rad.getScore(player) ?? 0;
+    let protection = radProtection.getScore(player) ?? 0;
+
+    // Condition
+    if (radiation >= 128 && protection < 12) {
+      player.applyDamage(2);
+    }
+  }
+}, 80);
