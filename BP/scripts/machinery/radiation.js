d5ade5ef04eac45c032d9ef48f807fc4d35d4d17
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-04-02 20:11:45 +0800

    Update radiation.js

--- BP/scripts/machinery/radiation.js
+++ BP/scripts/machinery/radiation.js
@@ -21,7 +21,7 @@ system.runInterval(() => {
 
     // Condition
     if (radiation >= 32 && protection < 12) {
-      player.applyDamage(1);
+      player.addEffect("nausea", 80, { amplifier: 0 });
     }
   }
 }, 400);
@@ -34,7 +34,20 @@ system.runInterval(() => {
 
     // Condition
     if (radiation >= 48 && protection < 12) {
-      player.applyDamage(1);
+      player.addEffect("nausea", 100, { amplifier: 0 });
+    }
+  }
+}, 360);
+system.runInterval(() => {
+  for (const player of world.getPlayers()) {
+
+    // Get scores safely
+    let radiation = rad.getScore(player) ?? 0;
+    let protection = radProtection.getScore(player) ?? 0;
+
+    // Condition
+    if (radiation >= 64 && protection < 12) {
+      player.addEffect("nausea", 120, { amplifier: 0 });
     }
   }
 }, 320);
@@ -46,24 +59,11 @@ system.runInterval(() => {
     let protection = radProtection.getScore(player) ?? 0;
 
     // Condition
-    if (radiation >= 64 && protection < 12) {
-      player.applyDamage(1);
-    }
-  }
-}, 240);
-system.runInterval(() => {
-  for (const player of world.getPlayers()) {
-
-    // Get scores safely
-    let radiation = rad.getScore(player) ?? 0;
-    let protection = radProtection.getScore(player) ?? 0;
-
-    // Condition
     if (radiation >= 80 && protection < 12) {
-      player.applyDamage(2);
+      player.addEffect("nausea", 140, { amplifier: 0 });
     }
   }
-}, 240);
+}, 280);
 system.runInterval(() => {
   for (const player of world.getPlayers()) {
 
@@ -73,10 +73,10 @@ system.runInterval(() => {
 
     // Condition
     if (radiation >= 96 && protection < 12) {
-      player.applyDamage(2);
+      player.addEffect("nausea", 160, { amplifier: 0 });
     }
   }
-}, 160);
+}, 240);
 system.runInterval(() => {
   for (const player of world.getPlayers()) {
 
@@ -86,7 +86,7 @@ system.runInterval(() => {
 
     // Condition
     if (radiation >= 128 && protection < 12) {
-      player.applyDamage(2);
+      player.addEffect("nausea", 200, { amplifier: 0 });
     }
   }
-}, 80);
+}, 200);
