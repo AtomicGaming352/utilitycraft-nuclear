edef1fd2fdd691b9f606917a12e1f745161c1f39
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-05-11 16:47:35 +0800

    Create isotope_stabilizer_mk1.js

---
+++ BP/recipes/blocks/machinery/machines/isotope_stabilizer_mk1.js
@@ -0,0 +1,45 @@
+{
+    "format_version": "1.20.80",
+    "minecraft:recipe_shaped": {
+        "description": {
+            "identifier": "neoutility:isotope_stabilizer_mk1"
+        },
+        "tags": [
+            "utilitycraft_workbench"
+        ],
+        "pattern": [
+            "LPL",
+            "DMD",
+            "LRL"
+        ],
+        "key": {
+            "R": {
+                "item": "utilitycraft:ryno_cooling_unit"
+            },
+            "L": {
+                "item": "utilitycraft:ryno_lead_plating"
+            },
+            "D": {
+                "item": "neoutility:stabilizer"
+            },
+            "M": {
+                "item": "utilitycraft:machine_case"
+            },
+            "P": {
+                "item": "utilitycraft:expert_chip"
+          }
+        },
+        "result": {
+            "item": "neoutility:isotope_stabilizer_mk1",
+            "count": 1
+        },
+        "unlock": [
+            {
+                "item": "utilitycraft:ryno_cooling_unit"
+            },
+            {
+                "item": "utilitycraft:ryno_lead_plate"
+            }
+        ]
+    }
+}
