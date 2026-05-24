c5b2d944ca12045c5d01ebe4ae2a3cf7d6ba3f6e
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-05-25 00:31:21 +0800

    Replace solid fuels with basic nuclear fuels in furnator

--- BP/scripts/machinery/generators/furnator.js
+++ BP/scripts/machinery/generators/furnator.js
@@ -1,7 +1,7 @@
 import { Generator, EnergyStorage } from "DoriosCore/machinery/index.js"
-import { solidFuels } from "../../config/recipes/fuel.js";
+import { basicnuclearFuels } from "../../config/recipes/reactor.js";
 
-DoriosAPI.register.blockComponent('furnator', {
+DoriosAPI.register.blockComponent('ryno_basic_reactor', {
     /**
      * Runs before the machine is placed by the player.
      * 
@@ -63,7 +63,7 @@ DoriosAPI.register.blockComponent('furnator', {
                     generator.displayEnergy()
                     return
                 }
-                const fuel = solidFuels.find(f => item?.typeId.includes(f.id));
+                const fuel = basicnuclearFuels.find(f => item?.typeId.includes(f.id));
                 if (!fuel) {
                     generator.setLabel(`
 §r§eInvalid Fuel
