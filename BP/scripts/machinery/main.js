5c21a2e031a74f961d9039a82817716842830675
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-05-25 00:24:40 +0800

    Update main.js

--- BP/scripts/machinery/main.js
+++ BP/scripts/machinery/main.js
@@ -1,7 +1,5 @@
 import "./generators/basic_reactor.js"
 import "./generators/coolant_reactor.js"
-import "./machines/noenergy.js"
-import "./DoriosMachinery/core.js"
 
 import "./transfer_system/system.js"
 import "./managers.js"
