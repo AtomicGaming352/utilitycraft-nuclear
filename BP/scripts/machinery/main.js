b4e275f2c9c1d0f69c350a9faf2c0940ac850410
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-05-25 15:49:53 +0800

    Fix import path for crusher_dual.js

--- BP/scripts/machinery/main.js
+++ BP/scripts/machinery/main.js
@@ -1,6 +1,6 @@
 import "./generators/basic_reactor.js"
 import "./generators/coolant_reactor.js"
   
-import "./machiness/crusher_dual.js"
+import "./machines/crusher_dual.js"
 
 import "./transfer_system/system.js"
