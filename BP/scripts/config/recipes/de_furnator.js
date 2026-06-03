ad6171ca165076561b7f477cec9a2a3a4c9eeac5
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-06-03 13:50:42 +0800

    Update de_furnator.js

--- BP/scripts/config/recipes/de_furnator.js
+++ BP/scripts/config/recipes/de_furnator.js
@@ -1,15 +1,14 @@
 export const coolantValues = Object.freeze({
-  'minecraft:ice': 400,
-  'minecraft:packed_ice': 800,
-  'minecraft:blue_ice': 2400,
+  'minecraft:ice': 2500,
+  'minecraft:packed_ice': 25000,
+  'minecraft:blue_ice': 50000,
   'utilitycraft:ryno_coolant_cell': 250000,
-  'minecraft:snowball': 50,
 });
 export const validCoolants = new Set(Object.keys(coolantValues));
 export function getCoolantValue(typeId) {
   return coolantValues[typeId] ?? 0;
 }
-export const deFurnatorRecipes = [
+export const newReactorRecipes = [
   {
     input: 'utilitycraft:ryno_nuclear_fuel',
     de: 2000000,
@@ -26,54 +25,19 @@ export const deFurnatorRecipes = [
   },
   {
     input: 'neoutility:fissiled_neptunium',
-    de: 100000,
+    de: 1920000,
     byproduct: 'neoutility:plutonium_bit',
     byproduct_amount: 1,
     byproduct_range: [0, 1],
   },
   {
-    input: 'minecraft:charcoal',
-    de: 900,
-    byproduct: 'minecraft:charcoal',
+    input: 'neoutility:neptunic_fuel',
+    de: 22560000,
+    byproduct: 'neoutility:plutonium_bit',
     byproduct_amount: 1,
-    byproduct_range: [1, 1],
-  },
-  {
-    input: 'minecraft:blaze_rod',
-    de: 2400,
-    byproduct: 'minecraft:blaze_powder',
-    byproduct_amount: 1,
-    byproduct_range: [1, 1],
-  },
-  {
-    input: 'minecraft:oak_log',
-    de: 300,
-    byproduct: 'minecraft:charcoal',
-    byproduct_amount: 1,
-    byproduct_range: [0, 1],
-  },
-  {
-    input: 'minecraft:spruce_log',
-    de: 300,
-    byproduct: 'minecraft:charcoal',
-    byproduct_amount: 1,
-    byproduct_range: [0, 1],
-  },
-  {
-    input: 'minecraft:birch_log',
-    de: 300,
-    byproduct: 'minecraft:charcoal',
-    byproduct_amount: 1,
-    byproduct_range: [0, 1],
-  },
-  {
-    input: 'minecraft:stick',
-    de: 80,
-    byproduct: 'minecraft:charcoal',
-    byproduct_amount: 1,
-    byproduct_range: [0, 1],
+    byproduct_range: [1, 2],
   },
 ];
 export const deFurnatorRecipeIndex = Object.fromEntries(
-  deFurnatorRecipes.map((recipe, index) => [recipe.input, index])
+  newReactorRecipes.map((recipe, index) => [recipe.input, index])
 );
